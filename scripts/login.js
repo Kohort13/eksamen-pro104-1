6bdf937679cc23f8c54fd7b8db6400008ed8166a
Maria Tan <mkvht@outlook.com>
2021-05-23 15:43:49 +0200

    fix remember me checkbox?

--- scripts/login.js
+++ scripts/login.js
@@ -1,5 +1,5 @@
 //Funksjon for login-knapp
-function validateUser(){
+function validateUser() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
@@ -9,5 +9,26 @@ function validateUser(){
     }
     else{
         alert("Feil brukernavn eller passord")
+    }
+}
+const rmCheck = document.getElementById("remember"), usernameInput = document.getElementById("username");
+
+if (localStorage.checkbox && localStorage.checkbox !== "") {
+    rmCheck.setAttribute("checked","checked");
+    usernameInput.value = localStorage.username;
+}else{
+    rmCheck.removeAttribute("checked");
+    usernameInput.value = "";
+}
+
+function rememberMe(){
+    if(rmCheck.checked && usernameInput.value !== "") {
+        localStorage.username = usernameInput.value;
+        localStorage.checkbox = rmCheck.value;
+        alert("is checked")
+    } else {
+        localStorage.username = "";
+        localStorage.checkbox = "";
+        alert("is not checked")
     }
 }
