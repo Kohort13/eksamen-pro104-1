14891a6bf454f049579936add93b47f04fe4fd19
Magnus Hodne <magnus@drobak.org>
2021-05-23 15:19:21 +0200

    Imported login script

--- scripts/login.js
+++ scripts/login.js
@@ -1,11 +1,9 @@
-
 //Funksjon for login-knapp
 function validateUser(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
     if(username == "user" && password == "user"){
-        alert("Login ok");
         window.location = "index.html"; //Sender deg til index siden om login ok.
         //return false;
     }
