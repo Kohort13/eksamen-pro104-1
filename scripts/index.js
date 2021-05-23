82a90d881782f156c6570bbf7068966e2d83f9f1
Maria Tan <mkvht@outlook.com>
2021-05-23 18:47:21 +0200

    Nå kan vi trykke oss videre til andre html sider :)

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,9 @@
 function menyer(){
     window.location = "meny-register.html";
 }
+function salgsoversikt(){
+    window.location = "salgsoversikt.html";
+}
+function ansatte(){
+    window.location = "ansatt-register.html";
+}
