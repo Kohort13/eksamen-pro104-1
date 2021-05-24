6ca862148fbe195bd08eb75f626df9bbcdb62637
Magnus Hodne <magnus@drobak.org>
2021-05-24 19:08:10 +0200

    Changed formatting of generated content on main page

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,45 @@
-
+import LoremModule from "./modules/LoremModule.js";
 //Funksjoner for index-siden
 function renderAnnouncement() {
     const lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, rerum.";
     let kunngjøringText = document.getElementById('kunngjøring');
+    renderProfits(kunngjøringText);
     renderFreeShifts(kunngjøringText);
 
-    renderProfits(kunngjøringText);
-    for (let i = 0; i < 100; i++) {
-        kunngjøringText.innerHTML +=`<a class="panel-block">
-                                <span class="panel-icon">
-                                    <i class="fas fa-pen" aria-hidden="true"></i>
-                                </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, amet!
-                            </a>` ;
+    for (let i = 0; i < 45; i++) {
+        kunngjøringText.innerHTML +=
+        `<a class="panel-block">
+            <span class="panel-icon has-text-info">
+                <i class="fas fa-comment" aria-hidden="true"></i>
+            </span>
+            <span>${LoremModule.getLorem()}</span>
+        </a>` ;
         
     }
                       
 }
 function renderProfits(output) {
-    output.innerHTML += `<a class="panel-block">
-                                <span class="panel-icon">
-                                    <i class="fas fa-pen" aria-hidden="true"></i>
-                                </span>Dagens omsetning
-                            </a>`;
+    // Solution for formatting number from https://stackoverflow.com/a/32355056
+    let profit = String(45000).replace(/(.)(?=(\d{3})+$)/g,'$1 ');
+
+    output.innerHTML += 
+    `<a class="panel-block">
+        <span class="panel-icon has-text-success">
+            <i class="fas fa-chart-line" aria-hidden="true"></i>
+        </span>
+        <span>Dagens omsetning: ${profit},-</span>
+    </a>`;
     
 }
 
 function renderFreeShifts(output) {
-    output.innerHTML += `<a class="panel-block is-active" >
-                                <span class="panel-icon">
-                                    <i class="fas fa-pen" aria-hidden="true"></i>
-                                </span>Ledige vakter!
-                            </a>`;
+    output.innerHTML += 
+    `<a class="panel-block">
+        <span class="panel-icon has-text-danger">
+            <i class="fas fa-exclamation-triangle" aria-hidden="true"></i>
+        </span>
+        <span>Noen vakter er ikke blitt tildelt!</span>
+    </a>`;
     
 }
 renderAnnouncement();
