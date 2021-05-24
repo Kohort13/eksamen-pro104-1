8ec379227e7287261497258fd8b595ef3ae4eebe
Magnus Hodne <magnus@drobak.org>
2021-05-25 00:57:20 +0200

    Implemented buttons for selecting period

--- scripts/salgsoversikt.js
+++ scripts/salgsoversikt.js
@@ -2,16 +2,35 @@ import SalgModule from "./modules/SalgModule.js";
 const columns = 3;
 const tableBody = document.getElementById("table-body");
 const tableHeader = document.getElementById("table-head");
-let children = document.getElementById("buttons").children;
-for(let i = 0; i < children.length; i++){
-    if(children[i].hasAttribute("class", "button"))
-        children[i].addEventListener("click", function() {setTimePeriod(children, this)});
-}
+
+const PeriodTypes = {DAY: 'day', WEEK: 'week', FREE: 'free'};
+let periodSelection = PeriodTypes.WEEK;
+
+const initButtons = (function(){
+    let children = document.getElementById("buttons").children;
+    for(let i = 0; i < children.length; i++){
+        if(children[i].hasAttribute("class", "button"))
+            children[i].addEventListener("click", function() {setTimePeriod(children, this)});
+    }
+}());
+
 function setTimePeriod(children, element){
+    const toDateField = document.getElementById("optional-to-date");
     for(let i = 0; i < children.length; i++){
         children[i].setAttribute("class", "button");
     }
-    element.setAttribute("class", "button is-active is-info");
+    element.setAttribute("class", "button is-info");
+    if(element.id === "period-day"){
+        toDateField.classList.add("is-hidden");
+        periodSelection = PeriodTypes.DAY;
+    }else if(element.id === "period-week"){
+        toDateField.classList.add("is-hidden");
+        periodSelection = PeriodTypes.WEEK;
+    }else if(element.id === "period-free-select"){
+        toDateField.classList.remove("is-hidden");
+
+        periodSelection = PeriodTypes.FREE;
+    }
 }
 function renderHeader(){
     let cells = "";
@@ -32,5 +51,8 @@ function renderTable(array){
         }
         tableBody.innerHTML += `<tr>${cells}</tr>`;
     }
+}
+function dateInput(){
+
 }
 renderTable(SalgModule.getAll());
