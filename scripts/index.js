00372d4f987d7fbf1da7a1ae13d851550b56afa3
Magnus Hodne <magnus@drobak.org>
2021-05-24 11:19:38 +0200

    Made cards tabbable and clickable

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,1 @@
-function menyer(){
-    window.location = "meny-register.html";
-}
-function salgsoversikt(){
-    window.location = "salgsoversikt.html";
-}
-function ansatte(){
-    window.location = "ansatt-register.html";
-}
+//Funksjoner for index-siden
