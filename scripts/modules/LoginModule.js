3926f757a22e3f5de31038b108960c6b3a5d3903
Magnus Hodne <magnus@drobak.org>
2021-05-25 20:26:25 +0200

    Added module for handling cookies

---
+++ scripts/modules/LoginModule.js
@@ -0,0 +1,39 @@
+const LoginModule = (function(){
+
+    //Cookie implementation from https://www.w3schools.com/js/js_cookies.asp
+    const setCookie = (name, value, expirydays) => {
+        var d = new Date();
+        d.setTime(d.getTime() + (expirydays*24*60*60*1000));
+        var expires = "expires=" + d.toUTCString();
+        document.cookie = name + "=" + value + ";" + expires + ";path=/";
+    }
+    const getCookie = (name) => {
+        var n = name + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(n) == 0) {
+                return c.substring(n.length, c.length);
+            }
+        }
+        return "";
+    }
+
+    function checkCookie() {
+        var username = getCookie("username");
+        if (username != "")
+            alert("Welcome again " + username);
+        else {
+            username = prompt("Please enter your name:", "");
+            if (username != "" && username != null)
+                setCookie("username", username, 365);
+        }
+    }
+    return {setCookie, getCookie, checkCookie};
+}());
+
+export default LoginModule;
