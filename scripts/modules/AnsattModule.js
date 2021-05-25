66ff9d6c5e957579195e2183826d0c8d152000c9
Magnus Hodne <magnus@drobak.org>
2021-05-25 13:33:37 +0200

    Extended functionality of AnsattModule

--- scripts/modules/AnsattModule.js
+++ scripts/modules/AnsattModule.js
@@ -1,5 +1,19 @@
 const AnsattModule = (function(){
 
+    const Positions = {
+        MANAGER: 'Avdelingsleder',
+        ASS_MANAGER: 'Assisterende avdelingsleder',
+        CHEF: 'Kokk',
+        ASS_CHEF: 'Assisterende kokk',
+        WAITER: 'Servitør'         
+    };
+    const Restaurants = {
+        RES_1: 'Restaurant 1', 
+        RES_2: 'Restaurant 2', 
+        RES_3: 'Restaurant 3', 
+        RES_4: 'Restaurant 4'
+    };
+
     class Ansatt {
         constructor(firstName, lastName, tel, email, addr, birthDate, salary, position, restaurant){
             this._firstName = firstName;
@@ -12,19 +26,23 @@ const AnsattModule = (function(){
             this._position = position;
             this._restaurant = restaurant;
         }
+        get fullName(){
+            return `${this._firstName} ${this._lastName}`;
+        }
     }
 
     const ansatte = [
-        new Ansatt("Ola", "Nordmensch", 98480551, "ola@nordmench.org", "Storgata 12, 0858 Oslo", "12.06.1978", "465 000kr", "Assisterende Kokk", "Restaurant 2"), 
-        new Ansatt("Trine", "Svenskamensch", 48983802, "trine@svensk.se", "Storgata 14, 0858 Oslo", "12.06.1989", "647 000kr", "Kokk", "Restaurant 2"), 
-        new Ansatt("Bob Kåre", "Johnniebjørn", 47985826, "bob@kåre.org", "Storgata 66, 0858 Oslo", "12.06.1996", "731 000kr", "Assisterende Avdelingssjef", "Restaurant 4")
+        new Ansatt("Ola", "Nordmensch", 98480551, "ola@nordmench.org", "Storgata 12, 0858 Oslo", "12.06.1978", "465 000kr", Positions.ASS_CHEF, Restaurants.RES_1), 
+        new Ansatt("Trine", "Svenskamensch", 48983802, "trine@svensk.se", "Storgata 14, 0858 Oslo", "12.06.1989", "647 000kr", Positions.CHEF, Restaurants.RES_2), 
+        new Ansatt("Bob Kåre", "Johnniebjørn", 47985826, "bob@kåre.org", "Storgata 66, 0858 Oslo", "12.06.1996", "731 000kr", Positions.ASS_MANAGER, Restaurants.RES_3)
     ];
 
     const getAll = () => ansatte;
     const getByPosition = (position) => {
         return ansatte.filter( ansatt => ansatt.position === position);
     }
-    return {getAll, getByPosition}
+    const getByIndex = (index) => { return ansatte[index]; }
+    return {getAll, getByPosition, getByIndex}
 }());
 
 export default AnsattModule;
