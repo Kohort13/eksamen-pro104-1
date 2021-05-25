e7634de60d0576f8a1bdedee4afc25c308a68c53
Maria Tan <mkvht@outlook.com>
2021-05-25 17:14:49 +0200

    Sortering på meny

--- scripts/meny-register.js
+++ scripts/meny-register.js
@@ -2,26 +2,40 @@ import VareModule from './modules/VareModule.js';
 
 let searchInput;
 let sortPriceBtn;
+let sortNameBtn;
+let sortIdBtn;
+
+let allegyNames = VareModule.getAllAllergies();
+
 function initialise(){
     searchInput = document.getElementById("search-input");
     searchInput.addEventListener('keyup', menuSearch);
     
     let tableTitles = `
-    <th class ="is-narrow"><a class="has-text-grey-dark" id = "prodId"><span>ID</span><span class = "icon"><i class="fas fa-caret-down"></i></span></a></th>
-    <th class ="is-narrow"><a class="has-text-grey-dark" id = "prodName"><span>Produkt Navn</span><span class="icon"><i class="fas fa-caret-down"></i></span></a></th>
+    <th class ="is-narrow"><a class="has-text-grey-dark" id = "prodId"></a></th>
+    <th class ="is-narrow"><a class="has-text-grey-dark" id = "prodName"></a></th>
     <th class ="is-narrow"><a class="has-text-grey-dark" id = "prodPrice"></a></th>
     <th colspan="10" class="has-text-centered">Allergener</th>`;
     let allergyTitles = `<td colspan="3"></td>`;
-    let allegyNames = VareModule.getAllAllergies();
+    
     allegyNames.forEach(name => {
         allergyTitles += `<td class="has-text-centered">${name}</td>`;
     });
     
     const menuHead = document.getElementById("menu-head");
     menuHead.innerHTML = `<tr>${tableTitles}</tr><tr>${allergyTitles}</tr>`;
+
     sortPriceBtn = document.getElementById("prodPrice");
     sortPriceBtn.innerHTML = `<span>Pris</span><span class = "icon"><i class="fas fa-caret-down"></i></span>`;
     sortPriceBtn.addEventListener('click', sortByPrice);
+
+    sortNameBtn = document.getElementById("prodName");
+    sortNameBtn.innerHTML = `<span>Produkt Navn</span><span class="icon"><i class="fas fa-caret-down"></i></span>`;
+    sortNameBtn.addEventListener('click', sortByName);
+
+    sortIdBtn = document.getElementById("prodId");
+    sortIdBtn.innerHTML =`<span>ID</span><span class = "icon"><i class="fas fa-caret-down"></i></span>`;
+    sortIdBtn.addEventListener('click', sortById);
 }
 initialise();
 
@@ -66,13 +80,38 @@ function sortByPrice(){
     let icon = sortPriceBtn.children[1].firstChild;
 
     if(icon.classList.contains("fa-caret-down")){
-        renderTable(VareModule.getSortedByPrice());
-        icon.classList.toggle("fa-caret-down", false);
-        icon.classList.toggle("fa-caret-up", true);
-    }else if(icon.classList.contains("fa-caret-up")){
         renderTable(VareModule.getSortedByPrice().reverse());
+        icon.classList.toggle("fa-caret-up", true);
+        icon.classList.toggle("fa-caret-down", false);
+    }else if(icon.classList.contains("fa-caret-up")){
+        renderTable(VareModule.getSortedByPrice());
         icon.classList.toggle("fa-caret-up", false);
         icon.classList.toggle("fa-caret-down", true);
     }
 }
+function sortByName(){
+    let icon = sortNameBtn.children[1].firstChild;
 
+    if(icon.classList.contains("fa-caret-down")){
+        renderTable(VareModule.getSortedByName().reverse());
+        icon.classList.toggle("fa-caret-up", true);
+        icon.classList.toggle("fa-caret-down", false);
+    }else if(icon.classList.contains("fa-caret-up")){
+        renderTable(VareModule.getSortedByName());
+        icon.classList.toggle("fa-caret-up", false);
+        icon.classList.toggle("fa-caret-down", true);
+    }
+}
+function sortById(){
+    let icon = sortIdBtn.children[1].firstChild;
+
+    if(icon.classList.contains("fa-caret-down")){
+        renderTable(VareModule.getSortedByID().reverse());
+        icon.classList.toggle("fa-caret-up", true);
+        icon.classList.toggle("fa-caret-down", false);
+    }else if(icon.classList.contains("fa-caret-up")){
+        renderTable(VareModule.getSortedByID());
+        icon.classList.toggle("fa-caret-up", false);
+        icon.classList.toggle("fa-caret-down", true);
+    }
+}
