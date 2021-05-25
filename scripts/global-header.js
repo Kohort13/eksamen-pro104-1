424638c3c726bc54033cc8d565eab6d54e0a449d
Magnus Hodne <magnus@drobak.org>
2021-05-25 13:57:03 +0200

    Cleaned up header test. Added js-file that will render it later

---
+++ scripts/global-header.js
@@ -0,0 +1,33 @@
+function renderHeader(){
+
+
+    //Legg inn kode som henter id fra siden her, og gjør innerHtml = ``;
+    //Boilerplate burger-implementation from Bulma.io
+    document.addEventListener('DOMContentLoaded', () => {
+
+        // Get all "navbar-burger" elements
+        const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+      
+        // Check if there are any navbar burgers
+        if ($navbarBurgers.length > 0) {
+      
+          // Add a click event on each of them
+          $navbarBurgers.forEach( el => {
+            el.addEventListener('click', () => {
+      
+              // Get the target from the "data-target" attribute
+              const target = el.dataset.target;
+              const $target = document.getElementById(target);
+      
+              // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+              el.classList.toggle('is-active');
+              $target.classList.toggle('is-active');
+      
+            });
+          });
+        }
+      
+      });
+    
+}
+renderHeader();
