689cef7decf0072caff2dab25e2f7d84413fd411
Magnus Hodne <magnus@drobak.org>
2021-05-28 17:30:52 +0200

    Changed import type of Bulma, can now edit colours

--- scripts/global-import.js
+++ scripts/global-import.js
@@ -1,7 +1,8 @@
 const importBulma = (function(){
     document.head.innerHTML += `
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <link rel="stylesheet" href="../mybulma/css/mystyles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
     `;
+    //<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
 
 }());
