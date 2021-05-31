e5be9ec2200f54641cd08bc8f408b6ba17ed87a7
Niloofar Razi <nilofarrazi@hotmail.com>
2021-05-31 09:51:46 +0200

    commenting the changes  in js document

--- scripts/documents.js
+++ scripts/documents.js
@@ -1,10 +1,14 @@
 //import DocumentsModule from "./modules/DocumentsModule.js"
+
+//cards in html contain the title and link to the .txt file
 let cards = "";
 let documentsList = [egenmeldingsskjema, permisjon, medatbeidersamtale, lovverk, personalrabatter,
      ferieoverføring, nyansatte, avviksskjema, varsling];
      documentsList.forEach(myFunction);
      document.getElementById("document-list").innerHTML = documentsList;
-function myFunction(value, index, array) {
+
+     //not sure if Array should have the ⬇ arrays name ??!!
+function myFunction(value, index, documentsList) {
   cards = cards + value + `<a href="${link}" download="${skjemanavn}.txt">
                         <div class="card-content">
                             <div class="block pt-7 has-text-centered">
